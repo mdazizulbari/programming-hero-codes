b965868b12b09656e21a34865f89546e58201925
smabari <mdazizulbarishafin@gmail.com>
2025-02-09 22:28:32 +0600

    done task 3

---
+++ 01. intro-to-js-tasks/task3.js
@@ -0,0 +1,7 @@
+// You task is to divide the given number by 5 and show the remainder as the output.
+
+let number = Number(prompt("Enter your number:"));
+let divisionRemainer = number % 5;
+console.log(
+  `The remainder after dividing your number by 5 is ${divisionRemainer}`
+);
