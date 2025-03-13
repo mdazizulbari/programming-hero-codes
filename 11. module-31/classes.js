01016362f6ef3fdaec12668a3bf2ddbc748ee136
smabari <mdazizulbarishafin@gmail.com>
2025-03-13 14:38:47 +0600

    extending class

--- 11. module-31/classes.js
+++ 11. module-31/classes.js
@@ -13,3 +13,33 @@ const iphone = new Product("iphone", "1234", "apple", "jobs");
 const samsung = new Product("samsung", "800", "samsung electronics", "idk");
 iphone.details();
 samsung.details();
+
+class Vehical {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+  move() {
+    console.log("I can go far");
+  }
+}
+
+class Bus extends Vehical {
+  constructor(name, price, seat) {
+    super(name, price);
+    this.seat = seat;
+  }
+  route() {
+    console.log("Dhaka to CoxBazar");
+  }
+}
+
+class Truck extends Vehical {
+  constructor(name, price, load) {
+    super(name, price);
+    this.load = load;
+  }
+}
+
+const greenLine = new Bus("greenLine", "12", 50)
+console.log(greenLine)
